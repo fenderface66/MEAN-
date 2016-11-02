602a7df317e69f1e6076c8ab5e30bb41f6116308
Robbie Heygate <r.heygate@gmail.com>
2016-11-02 16:51:13 +0000

    Added routing

---
+++ assets/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { MessagesComponent } from "./messages/messages.component";
+
+const APP_ROUTES: Routes = [
+  { path: 'messages',
+    component: MessagesComponent
+  }
+];
