96e316cec26aecbd7a2b405f8ccdb1148574a694
Lunch Train <lunch.train.readingroom@gmail.com>
2016-11-08 17:10:51 +0000

    http

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
-import { MessageInputComponent } from "./messages/message-input.component";;
+import { MessageInputComponent } from "./messages/message-input.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
@@ -14,25 +15,28 @@ import { LogoutComponent } from "./auth/logout.component";
 import { SignupComponent } from "./auth/signup.component";
 import { SigninComponent } from "./auth/signin.component";
 
-
-
 @NgModule({
     declarations: [
-        AppComponent, 
+        AppComponent,
         MessageComponent,
         MessageListComponent,
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-				LogoutComponent,
-				SignupComponent,
-				SigninComponent
+        LogoutComponent,
+        SignupComponent,
+        SigninComponent
     ],
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule ,routing],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing,
+        ReactiveFormsModule,
+        HttpModule
+    ],
     bootstrap: [AppComponent]
 })
-
 export class AppModule {
 
 }
--- assets/app/auth/logout.component.ts
+++ assets/app/auth/logout.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-	selector: 'app-logout',
-	template: `
-		<div class="col-md-8 col-md-offset-2">
-<button class="btn btn-danger" (click)="Logout">Logout</button>
-		</div>
-`
+    selector: 'app-logout',
+    template: `
+        <div class="col-md-8 col-md-offset-2">
+            <button class="btn btn-danger" (click)="onLogout()">Logout</button>
+        </div>
+    `
 })
 export class LogoutComponent {
-	onLogout() {
-		
-	}
+    onLogout() {
+
+    }
 }
